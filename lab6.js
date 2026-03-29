8bd35377eca53b99b3ff40e0d79623855527bb86
Semin Kim <ksm340242@gmail.com>
2026-03-28 23:36:50 -0400

    lab6

--- lab6.js
+++ lab6.js
@@ -2,18 +2,24 @@ function loadData() {
     fetch("https://brianobruno.github.io/cats.json")
         .then(response => response.json())
         .then(data => {
-            data.facts.sort(function(a, b) {
+            let facts = data.facts || [];
+            facts.sort(function(a, b) {
                 return a.factId - b.factId;
             });
             let tableBody = document.getElementById("tableBody");
             tableBody.innerHTML = "";
-            for (let i = 0; i < data.facts.length; i++) {
+            for (let i = 0; i < facts.length; i++) {
                 tableBody.innerHTML +=
                     "<tr>" +
-                    "<td>" + data.facts[i].factId + "</td>" +
-                    "<td>" + data.facts[i].fact + "</td>" +
+                    "<td>" + facts[i].factId + "</td>" +
+                    "<td>" + facts[i].fact + "</td>" +
                     "</tr>";
             }
-            document.getElementById("catImage").src = data.catPhoto;
+            if (data.catPhoto) {
+                document.getElementById("catImage").src = data.catPhoto;
+            }
+        })
+        .catch(error => {
+            console.log("Error:", error);
         });
 }
