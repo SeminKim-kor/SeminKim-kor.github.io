2c7646dadfea00b59e5c2fefa71eeb9587595181
Semin Kim <ksm340242@gmail.com>
2026-05-07 03:09:30 -0400

    Spotify API Final Project

--- finalProject/script.js
+++ finalProject/script.js
@@ -1,5 +1,6 @@
 let clientId = "69d5cebd602545138a449865d1803dd9";
 let redirectUri = "http://127.0.0.1:5500/finalProject/index.html";
+checkForCode();
 async function login() {
     let codeVerifier = makeRandomString(64);
     let codeChallenge = await makeCodeChallenge(codeVerifier);
@@ -28,6 +29,39 @@ async function makeCodeChallenge(codeVerifier) {
         .replace(/\+/g, "-")
         .replace(/\//g, "_")
         .replace(/=+$/, "");
+}
+async function checkForCode() {
+    let params = new URLSearchParams(window.location.search);
+    let code = params.get("code");
+
+    if (code != null) {
+        await getAccessToken(code);
+    }
+}
+async function getAccessToken(code) {
+    let codeVerifier = localStorage.getItem("code_verifier");
+    let bodyData = "client_id=" + clientId;
+    bodyData += "&grant_type=authorization_code";
+    bodyData += "&code=" + code;
+    bodyData += "&redirect_uri=" + encodeURIComponent(redirectUri);
+    bodyData += "&code_verifier=" + codeVerifier;
+    let response = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: bodyData
+    });
+    let data = await response.json();
+    if (data.access_token) {
+        localStorage.setItem("access_token", data.access_token);
+
+        document.getElementById("message").innerHTML = "Login successful.";
+
+        window.history.replaceState({}, document.title, "index.html");
+    } else {
+        document.getElementById("message").innerHTML = "Login failed.";
+    }
 }
 function searchSong() {
     let userSearch = document.getElementById("searchInput").value;
